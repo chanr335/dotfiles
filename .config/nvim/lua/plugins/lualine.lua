f706e196bc5b3fb9ba2f3111eabd13f277a5f406
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-01-05 02:04:43 -0500

    slight lualine n tmux changes

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,9 +1,16 @@
-return{
-	{
-	    'nvim-lualine/lualine.nvim',
-	    dependencies = { 'nvim-tree/nvim-web-devicons' },
-	    opts = {
-		},
-	}
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+    --load on setup
+      require('lualine').setup {
+        options = {
+          theme = 'auto',
+          component_separators= { left = '󰇻'},
+        }
+      }
+    end
+  }
 }
 
