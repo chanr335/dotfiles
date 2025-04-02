13332bb2ebdff1e1a609b86b3140d304131407a9
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-04-02 00:04:29 -0400

    turbo on write

---
+++ .config/nvim/lua/koge/turbo.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+local function play_sound()
+    local sound_path = vim.fn.expand("~") .. "/dotfiles/.config/nvim/lua/koge/sounds/turbo.wav"
+    local cmd = string.format("mpv --no-video --no-terminal %s &", sound_path)
+    os.execute(cmd)
+end
+
+function M.setup()
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        pattern = "*",
+        callback = function()
+            play_sound()
+        end,
+    })
+end
+
+return M
