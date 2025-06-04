c43a5da9e7d3abafe3fea2386621f9e2032c459d
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-06-03 23:59:30 -0400

    remove bufferline keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,10 @@
 local keymap = vim.keymap -- for conciseness
 
-vim.keymap.set('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
-
---bufferline move between tabs
-vim.keymap.set("n", "H", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "L", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
-
-vim.keymap.set("n", "<leader>q", ":bdelete<CR>", { noremap = true, silent = true })
+-- vim.keymap.set('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
+--
+-- --bufferline move between tabs
+-- vim.keymap.set("n", "H", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "L", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+--
+-- vim.keymap.set("n", "<leader>q", ":bdelete<CR>", { noremap = true, silent = true })
 
